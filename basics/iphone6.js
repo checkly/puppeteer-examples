97c5226e1e309e8040fec09adfc34a911e1f0e52
Tim Nolet <tim@magnetic.io>
2017-10-05 10:44:40 +0200

    first test

---
+++ basics/iphone6.js
@@ -0,0 +1,10 @@
+const puppeteer = require('puppeteer')
+const devices = require('puppeteer/DeviceDescriptors')
+const browser = await puppeteer.launch()
+const page = await browser.newPage()
+await page.emulate(devices['iPhone 6'])
+await page.goto('https://google.com/')
+await page.screenshot({path: 'full.png', fullPage: true})
+console.log(await page.title())
+await browser.close()
+
