5d3de2ce0ce6a3d27eae1f20912a6f7a7ab20952
Tim Nolet <timnolet@hotmail.com>
2018-02-04 14:03:31 +0100

    removes tab switch

--- a. mocha-tests/gmail.js
+++ a. mocha-tests/gmail.js
@@ -9,7 +9,7 @@ let browser
 let page
 
 before(async () => {
-  browser = await puppeteer.launch({ headless: false })
+  browser = await puppeteer.launch()
   page = await browser.newPage()
 })
 
@@ -20,17 +20,6 @@ describe('Check Gmail signup', () => {
     const SignUpButton = await page.$('a.hero_home__link__desktop')
     assert.ok(SignUpButton)
   }).timeout(10000)
-
-  it('Signup form opens in a new tab', async () => {
-    await page.click('a.hero_home__link__desktop')
-    const pages = await browser.pages()
-    console.log(pages)
-    const newPage = pages[1]
-    await newPage.waitForSelector('div.sign-up')
-    const form = await newPage.$('div.sign-up')
-    await newPage.screenshot({ path: 'gmail.png' })
-    assert.ok(form)
-  }).timeout(30000)
 })
 
 after(async () => {
