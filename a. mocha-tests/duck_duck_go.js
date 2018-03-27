32b443e6407b976434fed0e1e13a51bb475af2ec
Tim Nolet <timnolet@hotmail.com>
2018-03-27 16:04:32 +0200

    adds Duck Duck Go mocha test

---
+++ a. mocha-tests/duck_duck_go.js
@@ -0,0 +1,33 @@
+/**
+ * @name Duck Duck Go search
+ * @desc Goes to duckduckgo.com, searches for "chrome puppeteer", asserts the result and snaps a screenshots
+ */
+
+const assert = require('assert')
+const puppeteer = require('puppeteer')
+
+let browser
+let page
+
+before(async () => {
+  browser = await puppeteer.launch()
+  page = await browser.newPage()
+})
+
+describe('Duck Duck Go Search', () => {
+  it('returns Chrome Puppeteer Github repo as first search result"', async () => {
+
+    await page.goto('https://duckduckgo.com/', { waitUntil: 'networkidle2' })
+    await page.type('input#search_form_input_homepage', 'chrome puppeteer', { delay: 50 })
+    await page.click('input#search_button_homepage')
+    await page.waitForSelector('.results--main #r1-0')
+
+    const githubLink = await page.evaluate(() => document.querySelector('a.result__a').textContent.trim())
+    assert(githubLink, 'https://github.com/GoogleChrome/puppeteer')
+    await page.screenshot({ path: 'duckduckgo.png' })
+  }).timeout(10000)
+})
+
+after(async () => {
+  await browser.close()
+})
