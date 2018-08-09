2673bd2e96a6eb4dbdda0cc4050aaf064bde968b
Tim Nolet <timnolet@hotmail.com>
2018-08-09 13:59:06 +0200

    fix: fixes broken test due to new walmart layout

--- a. mocha-tests/walmart.js
+++ a. mocha-tests/walmart.js
@@ -17,14 +17,14 @@ describe('Walmart shopping cart', () => {
   it('shows the correct product', async () => {
     await page.setViewport({ width: 1280, height: 800 })
     await page.goto('https://www.walmart.com/ip/Super-Mario-Odyssey-Nintendo-Switch/56011600', { waitUntil: 'networkidle2' })
-    const productTitle = await page.evaluate(() => document.querySelector('h1.prod-ProductTitle').textContent)
-    assert.equal(productTitle, 'Super Mario Odyssey (Nintendo Switch)')
+    const productTitle = await page.$eval('h1.prod-ProductTitle', txt => txt.textContent)
+    assert.equal(productTitle, 'Super Mario Odyssey, Nintendo, Nintendo Switch, 045496590741')
   }).timeout(20000)
 
   it('adds the product to the cart', async () => {
     await page.click('button.prod-ProductCTA--primary')
-    await page.waitForSelector('.Cart-PACModal-ItemInfoContainer')
-    const quantity = await page.evaluate(() => document.querySelector('a.OrderSummary-SubTotal-itemCountLink').textContent)
+    await page.waitForSelector('.copy-mini.pos-item-qty')
+    const quantity = await page.$eval('.copy-mini.pos-item-qty', txt => txt.textContent)
     assert.equal(quantity, '(1 item)')
   }).timeout(10000)
 })
