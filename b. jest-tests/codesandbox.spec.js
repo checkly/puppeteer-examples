68ed6e326b60734317e926d5dcb0672559d1425c
Tim Nolet <timnolet@hotmail.com>
2018-08-10 18:47:52 +0200

    feat: adds a codesandbox.io test for Mocha and Jest test runners

---
+++ b. jest-tests/codesandbox.spec.js
@@ -0,0 +1,32 @@
+/**
+ * @name codesandbox.io
+ * @desc Goes to codesandbox.io, creates a new sandbox and selects the Vue.js template
+ */
+const puppeteer = require('puppeteer')
+
+let browser
+let page
+
+beforeAll(async () => {
+  browser = await puppeteer.launch({ headless: true })
+  page = await browser.newPage()
+})
+
+describe('codesandbox.io', () => {
+  test('creates a Vue.js sandbox', async () => {
+    await page.setViewport({ width: 1280, height: 800 })
+    await page.goto('https://codesandbox.io/', { waitUntil: 'networkidle2' })
+    await page.waitForSelector('a[href="/s"]')
+    await page.click('a[href="/s"]')
+    await page.waitForSelector('a[href="/s/vue"]')
+    await page.click('a[href="/s/vue"]')
+    await page.waitForSelector('.react-monaco-editor-container')
+    const editor = await page.$('.react-monaco-editor-container')
+    await page.screenshot({ path: 'codesandbox_vanillajs.png' })
+    expect(editor).toBeTruthy()
+  }, 30000)
+})
+
+afterAll(async () => {
+  await browser.close()
+})
