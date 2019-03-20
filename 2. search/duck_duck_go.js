a77ff0f898176487c9efbd05bd59966414bdc992
Tim Nolet <timnolet@hotmail.com>
2019-03-20 13:48:13 +0100

    feat: adds another duck duck go example

---
+++ 2. search/duck_duck_go.js
@@ -0,0 +1,15 @@
+/**
+ * @name Duck Duck Go search
+ */
+
+const puppeteer = require('puppeteer')
+
+(async () => {
+  const browser = await puppeteer.launch()
+  const page = await browser.newPage()
+  await page.goto('https://duckduckgo.com/', { waitUntil: 'networkidle2' })  
+  await page.type('#search_form_input_homepage', 'Puppeteer')
+  const searchValue = await page.$eval('#search_form_input_homepage', el => el.value)
+  console.log(searchValue)
+  await browser.close()
+})()
