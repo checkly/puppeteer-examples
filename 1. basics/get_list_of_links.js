c9306740d6061b14939a91115d5ad2698b479a11
Tim Nolet <timnolet@hotmail.com>
2019-03-20 13:41:49 +0100

    fix: remove tracing

--- 1. basics/get_list_of_links.js
+++ 1. basics/get_list_of_links.js
@@ -8,12 +8,10 @@ const puppeteer = require('puppeteer');
 (async () => {
   const browser = await puppeteer.launch()
   const page = await browser.newPage()
-  await page.tracing.start({path: 'trace.json', categories: ['devtools.timeline']})
   await page.goto('https://news.ycombinator.com/news')
 
   // execute standard javascript in the context of the page.
   const stories = await page.$$eval('a.storylink', anchors => { return anchors.map(anchor => anchor.textContent).slice(0, 10) })
   console.log(stories)
-  await page.tracing.stop();
   await browser.close()
 })()
