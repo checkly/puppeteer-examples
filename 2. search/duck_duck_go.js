12a504020ad6378daa7a22d8310a1ba6018d82a7
dietbald <thibault.judicq@gmail.com>
2019-10-12 16:40:24 +0700

    Run search and return basic results

--- 2. search/duck_duck_go.js
+++ 2. search/duck_duck_go.js
@@ -9,7 +9,46 @@ const puppeteer = require('puppeteer')
   const page = await browser.newPage()
   await page.goto('https://duckduckgo.com/', { waitUntil: 'networkidle2' })  
   await page.type('#search_form_input_homepage', 'Puppeteer')
-  const searchValue = await page.$eval('#search_form_input_homepage', el => el.value)
-  console.log(searchValue)
+  
+  //Start search
+  await page.keyboard.press("Enter");
+
+  //give the page 3seconds to load
+  await page.waitFor(3000);
+
+  //make sure it is loaded
+  await page.waitForSelector('div[class="results--main"]');
+
+  //parse results
+  var results = await page.evaluate(() => {
+    let searchResults = [];
+    let elms = document.querySelectorAll(
+      'div[class="result__body links_main links_deep"]'
+    );
+    console.log("elements" + elms);
+    elms.forEach(el => {
+      var element = el.querySelector('h2 > a[class="result__a"]');
+
+      var isAd = el.querySelectorAll('span[class="badge--ad-wrap"]').length > 0;
+
+      var description = el.querySelector(
+        'div[class="result__snippet js-result-snippet"]'
+      ).textContent;
+
+      var searchResult = {
+        title: element.textContent,
+        link: element.href,
+        isAd: isAd,
+        description: description
+      };
+      searchResults.push(searchResult);
+    });
+
+    return searchResults;
+  });
+
+  //print found results
+  console.log(results);
+  
   await browser.close()
 })()
