fa0b7bb58c9d68838356927d6b6a3676b7e3ca38
Umut Uzgur <umutuzgur@gmail.com>
2020-03-31 16:48:04 +0200

    fix: update the emulate_devices example

--- 1. basics/emulate_devices.js
+++ 1. basics/emulate_devices.js
@@ -7,14 +7,17 @@
  * @see {@link https://github.com/GoogleChrome/puppeteer/blob/master/docs/api.md#pageemulateoptions}
  */
 const puppeteer = require('puppeteer')
-const devices = require('puppeteer/DeviceDescriptors');
+const iPhone = puppeteer.devices['iPhone 6'];
 
 (async () => {
   const browser = await puppeteer.launch()
   const page = await browser.newPage()
-  await page.emulate(devices['iPhone 6'])
+  await page.emulate(iPhone)
   await page.goto('https://google.com/')
-  await page.screenshot({path: 'full.png', fullPage: true})
+  await page.screenshot({
+    path: 'full.png',
+    fullPage: true
+  })
   console.log(await page.title())
   await browser.close()
 })()
